f29b4cb504fc08daa9408cc1eaaa184b974c4d61
tiffany-meng <tmeng@ucsd.edu>
2021-04-11 18:41:49 -0700

    assignment 2

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -19,8 +19,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         // Do any additional setup after loading the view.
         
-        print("Hello")
-        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -34,7 +32,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 self.movies = dataDictionary["results"] as! [[String:Any]]
             
                 self.tableView.reloadData()
-                print(dataDictionary)
 
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
@@ -65,6 +62,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.posterView.af_setImage(withURL: posterUrl)
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("loadin g up the details screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 
